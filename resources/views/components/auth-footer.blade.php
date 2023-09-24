ab896c5a430d31d614ac309be775203c2507f47c
Edi Gunawan <gunawan.eddy1@gmail.com>
2023-09-24 13:44:32 +0700

    Tugas 2 Dashboard Login Register Resset password

--- resources/views/components/auth-footer.blade.php
+++ resources/views/components/auth-footer.blade.php
@@ -1,3 +1,3 @@
   <div class="simple-footer">
-      Copyright &copy; Stisla 2018
+      Copyright FIC8 &copy; Jago Flutter 2023
   </div>
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,22 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     // return view('welcome');
-    return view('pages.blank-page', ['type_menu'=>'']);
+    // return view('pages.blank-page', ['type_menu'=>'']);
+    return view('pages.app.dashboard-siakad', ['type_menu'=>'']);
  });
+
+ Route::get('/login', function () {
+    return view('pages.auth.auth-login');
+ })->name('login');
+
+ Route::get('/register', function () {
+    return view('pages.auth.auth-register');
+ })->name('regsiter');
+
+ Route::get('/forgot', function () {
+    return view('pages.auth.auth-forgot-password');
+ })->name('forgot');
+
+ Route::get('/resset-password', function () {
+    return view('pages.auth.auth-reset-password');
+ })->name('forgot');
